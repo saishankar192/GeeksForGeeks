9649b5a1ee48bacfeb5a9b3ddd5926af2855a755
Sai Shankar <saishankar192@gmail.com>
2022-02-05 17:56:35 +0530

    Solved Day 3 challenge

--- February Challenges/Day-3 Excel Sheet.cpp
+++ February Challenges/Day-3 Excel Sheet.cpp
@@ -2,6 +2,20 @@ class Solution{
     public:
     string ExcelColumn(int N)
     {
-        
+        string ans ="";
+        while(N){
+            int rem = N%26;
+            if(rem==0){
+                ans = 'Z'+ans;
+                N = ((N/26) -1);
+            }
+            else{
+                char c = 'A'+rem-1;
+                ans = c + ans;
+                N/=26;
+            }
+        }
+
+        return ans;
     }
 };
